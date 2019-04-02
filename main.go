b6644d0511b4838284e25435abd5cf37fb85187f
Ben Hundley <ben.hundley@gmail.com>
2019-04-02 15:54:16 -0500

    Use correct handler func

--- main.go
+++ main.go
@@ -3,106 +3,106 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"math/rand"
 	"net/http"
-  "math/rand"
-  "time"
-  "strings"
-  "os"
-  "path"
+	"os"
+	"path"
+	"strings"
+	"time"
 
-  "github.com/aws/aws-sdk-go/service/kinesis"
-  "github.com/aws/aws-sdk-go/aws"
-  "github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/kinesis"
 )
 
 const (
-  kinesisStreamName = "my-stream"
-  logFileName = "logs.json"
+	kinesisStreamName = "my-stream"
+	logFileName       = "logs.json"
 )
 
 func init() {
-    rand.Seed(time.Now().UnixNano())
+	rand.Seed(time.Now().UnixNano())
 }
 
 var letterRunes = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
 
 func randomString(n int) string {
-    b := make([]rune, n)
-    for i := range b {
-        b[i] = letterRunes[rand.Intn(len(letterRunes))]
-    }
-    return string(b)
+	b := make([]rune, n)
+	for i := range b {
+		b[i] = letterRunes[rand.Intn(len(letterRunes))]
+	}
+	return string(b)
 }
 
 func writeLogsToFile(logs []json.RawMessage) error {
-  // Use current directory to write log file.
-  dir, err := os.Getwd()
-  if err != nil {
-    return err
-  }
+	// Use current directory to write log file.
+	dir, err := os.Getwd()
+	if err != nil {
+		return err
+	}
 
-  filename := path.Join(dir, logFileName)
-  f, err := os.OpenFile(filename, os.O_APPEND|os.O_WRONLY, 0600)
-  if err != nil {
-    return err
-  }
+	filename := path.Join(dir, logFileName)
+	f, err := os.OpenFile(filename, os.O_APPEND|os.O_WRONLY, 0600)
+	if err != nil {
+		return err
+	}
 
-  defer f.Close()
+	defer f.Close()
 
-  logOut, err := json.Marshal(logs)
-  if err != nil {
-    return err
-  }
+	logOut, err := json.Marshal(logs)
+	if err != nil {
+		return err
+	}
 
-  if _, err = f.Write(logOut); err != nil {
-    return err
-  }
+	if _, err = f.Write(logOut); err != nil {
+		return err
+	}
 
-  return nil
+	return nil
 }
 
 func getKinesisClient() *kinesis.Kinesis {
-  sess := session.Must(session.NewSession())
-  return kinesis.New(sess)
+	sess := session.Must(session.NewSession())
+	return kinesis.New(sess)
 }
 
 func sendLogsToKinesis(logs []json.RawMessage) error {
-  k := getKinesisClient()
+	k := getKinesisClient()
 
-  putRecords := []*kinesis.PutRecordsRequestEntry{}
-  for _, log := range logs {
-    // TODO not certain this will work for partition key without testing. Would
-    // be ideal to use ID parsed from each log, if there is one.
-    partitionKey := randomString(128)
+	putRecords := []*kinesis.PutRecordsRequestEntry{}
+	for _, log := range logs {
+		// TODO not certain this will work for partition key without testing. Would
+		// be ideal to use ID parsed from each log, if there is one.
+		partitionKey := randomString(128)
 
-    putRecord := &kinesis.PutRecordsRequestEntry{
-      PartitionKey: aws.String(partitionKey),
-      Data: log,
-    }
-    putRecords = append(putRecords, putRecord)
-  }
+		putRecord := &kinesis.PutRecordsRequestEntry{
+			PartitionKey: aws.String(partitionKey),
+			Data:         log,
+		}
+		putRecords = append(putRecords, putRecord)
+	}
 
-  input := &kinesis.PutRecordsInput{
-    StreamName: aws.String(kinesisStreamName),
-    Records: putRecords,
-  }
+	input := &kinesis.PutRecordsInput{
+		StreamName: aws.String(kinesisStreamName),
+		Records:    putRecords,
+	}
 
-  out, err := k.PutRecords(input)
-  if err != nil {
-    return err
-  }
+	out, err := k.PutRecords(input)
+	if err != nil {
+		return err
+	}
 
-  failedRecords := *out.FailedRecordCount
-  if failedRecords > 0 {
-    errorMessages := []string{}
-    for _, record := range out.Records {
-      errorMessages = append(errorMessages, *record.ErrorMessage)
-    }
-    errorMessagesStr := strings.Join(errorMessages, "\n")
-    return fmt.Errorf("Kinesis submission failed for %d records:\n%s", failedRecords, errorMessagesStr)
-  }
+	failedRecords := *out.FailedRecordCount
+	if failedRecords > 0 {
+		errorMessages := []string{}
+		for _, record := range out.Records {
+			errorMessages = append(errorMessages, *record.ErrorMessage)
+		}
+		errorMessagesStr := strings.Join(errorMessages, "\n")
+		return fmt.Errorf("Kinesis submission failed for %d records:\n%s", failedRecords, errorMessagesStr)
+	}
 
-  return nil
+	return nil
 }
 
 func handlePostLogs(w http.ResponseWriter, r *http.Request) {
@@ -116,7 +116,7 @@ func handlePostLogs(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-  // Parse logs into slice. We don't need to parse each item (yet).
+	// Parse logs into slice. We don't need to parse each item (yet).
 	logs := []json.RawMessage{}
 	err := json.NewDecoder(r.Body).Decode(logs)
 	if err != nil {
@@ -124,23 +124,21 @@ func handlePostLogs(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-  // Send logs to Kinesis
-  if err := sendLogsToKinesis(logs); err != nil {
-    http.Error(w, err.Error(), 500)
-    return
-  }
+	// Send logs to Kinesis
+	if err := sendLogsToKinesis(logs); err != nil {
+		http.Error(w, err.Error(), 500)
+		return
+	}
 
-  // Write logs to disk
-  if err := writeLogsToFile(logs); err != nil {
-    http.Error(w, err.Error(), 500)
-    return
-  }
+	// Write logs to disk
+	if err := writeLogsToFile(logs); err != nil {
+		http.Error(w, err.Error(), 500)
+		return
+	}
 }
 
 func main() {
-	http.HandleFunc("/logs", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Welcome to my website!")
-	})
+	http.HandleFunc("/logs", handlePostLogs)
 
 	http.ListenAndServe(":8080", nil)
 }
